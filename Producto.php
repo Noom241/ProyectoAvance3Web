99923b3db75c8e0f1d2a4da6f150024216e12f49
Noom <joaquin12345.007.2002@gmail.com>
2023-10-25 00:07:48 -0500

    Act

--- Producto.php
+++ Producto.php
@@ -1,87 +1,110 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php
+$host = 'roundhouse.proxy.rlwy.net';
+$port = 36354;
+$user = 'root';
+$password = 'drmy$5c@-f-_7eq7$wr8k@0n4lzv319$';
+$database = 'railway';
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
-    <link rel="stylesheet" href="shop/styles.css">
-    <title>Sidebar con Bootstrap</title>
-</head>
+$mysqli = new mysqli($host, $user, $password, $database, $port);
 
-<body>
-    <?php
-include 'navbar.php';
+if ($mysqli->connect_error) {
+    die("La conexión a la base de datos falló: " . $mysqli->connect_error);
+}
+
+// Realizar una consulta para obtener todos los productos
+$query = "SELECT * FROM muebles";
+$result = $mysqli->query($query);
+
+if (!$result) {
+    die("Error al recuperar datos: " . $mysqli->error);
+}
 ?>
 
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Productos</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="shop/styles.css">
+</head>
+<body class="bg-dark">
+<?php
+include 'navbar.php';
+?>
+    <div class="sidebar bg-dark text-white col-md-3">
+        <!-- Barra de búsqueda -->
+        <label >Buscar:</label>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Buscar productos" id="searchInput">
+        </div>
 
+        <!-- Barra de categorías -->
+        <div class="form-group">
+            <label for="material">Material:</label>
+            <select id="material" class="form-control">
+                <option value="todos">Todos</option>
+                <option value="Madera">Madera</option>
+                <option value="Metal">Metal</option>
+                <option value="Vidrio">Vidrio</option>
+            </select>
+        </div>
 
-    <div class="row w-100 p-0 m-0">
-        <div class="col-md-2 bg-dark">
-            <!-- Barra de filtros -->
-            <div class="card border-dark bg-dark text-white " style="width: 300px; height: 100vh">
-                <div class="card-body">
-                    <h4 class="card-title">Filtros</h4>
-                    <div class="form-group">
-                        <input type="text" id="busqueda" class="form-control" placeholder="Buscar por nombre">
-                    </div>
-                    <div class="form-group">
-                        <label for="material">Material:</label>
-                        <select id="material" class="form-control">
-                            <option value="todos">Todos</option>
-                            <option value="Madera">Madera</option>
-                            <option value="Metal">Metal</option> <!-- Agrega más opciones según tus materiales -->
-                            <option value="Vidrio">Vidrio</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="estilo">Estilo:</label>
-                        <select id="estilo" class="form-control">
-                            <option value="todos">Todos</option>
-                            <option value="Clásico">Clásico</option>
-                            <option value="Moderno">Moderno</option> <!-- Agrega más opciones según tus estilos -->
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="color">Color:</label>
-                        <select id="color" class="form-control">
-                            <option value="todos">Todos</option>
-                            <option value="Marrón">Marrón</option>
-                            <option value="Negro">Negro</option> <!-- Agrega más opciones según tus colores -->
-                            <option value="Blanco">Blanco</option>
-                            <option value="Plomo">Plomo</option>
-                            <option value="Rosado">Rosado</option>
-                            <option value="Café">Café</option>
-                            <option value="Verde">Verde</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="tipo">Tipo:</label>
-                        <select id="tipo" class="form-control">
-                            <option value="todos">Todos</option>
-                            <option value="Silla">Silla</option>
-                            <option value="Mesa">Mesa</option>
-                            <option value="Sofá">Sofá</option> <!-- Agrega más opciones según tus tipos -->
-                        </select>
+        <div class="form-group">
+            <label for="estilo">Estilo:</label>
+            <select id="estilo" class="form-control">
+                <option value="todos">Todos</option>
+                <option value="Clásico">Clásico</option>
+                <option value="Moderno">Moderno</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="color">Color:</label>
+            <select id="color" class="form-control">
+                <option value="todos">Todos</option>
+                <option value="Marrón">Marrón</option>
+                <option value="Negro">Negro</option>
+                <option value="Blanco">Blanco</option>
+                <option value="Plomo">Plomo</option>
+                <option value="Rosado">Rosado</option>
+                <option value="Café">Café</option>
+                <option value="Verde">Verde</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="tipo">Tipo:</label>
+            <select id="tipo" class="form-control">
+                <option value="todos">Todos</option>
+                <option value="Silla">Silla</option>
+                <option value="Mesa">Mesa</option>
+                <option value="Sofá">Sofá</option>
+            </select>
+        </div>
+    </div>
+    <div class="products bg-primary col-md-9 p-5">
+        <!-- Lista de productos -->
+        <div class="row w-100" id="productList">
+            <?php while ($row = $result->fetch_assoc()): ?>
+                <div class="col-lg-4 col-md-6 mb-4 product-item" data-material="<?php echo $row['material']; ?>" data-estilo="<?php echo $row['estilo']; ?>" data-color="<?php echo $row['color']; ?>" data-tipo="<?php echo $row['tipo']; ?>">
+                    <div class="card">
+                        <img src="<?php echo $row['imagenURL']; ?>" class="card-img-top" alt="Imagen del producto">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $row['nombre']; ?></h5>
+                            <p class="card-text">Precio: $<?php echo $row['precio']; ?></p>
+                            <button class="btn btn-primary addToCart" data-product-name="<?php echo $row['nombre']; ?>" data-product-price="<?php echo $row['precio']; ?>">Añadir al carrito</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="col-md-10 bg-info">
-            <div class="main-content">
-                <!-- Productos en 4 columnas -->
-
-                <!-- Repite esta estructura para los otros productos -->
-            </div>
+            <?php endwhile; ?>
         </div>
     </div>
 
-    <!-- Scripts de Bootstrap y Font Awesome -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
     <script src="shop/main.js"></script>
-</body>
 
+
+</body>
 </html>
